78234463e2f21a5377687d1428951ed4daac26e9
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-03-04 22:45:38 +0530

    view buttons and timeout checks

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Viswanath');
+
+  setTimeout(async => {
+    setName('Viswanath Sarma');
+  },10000);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         {/*<View>
           <Text>How is your Day</Text>
         </View>//this expected to inherit the parent view style header*/}
-        <Text style={styles.textArea}>Welcome to the next step of career with React-Native! A big hello Viswanath.</Text>
+        <Text style={styles.textArea}>Welcome to the next step of career with React-Native! A big hello {name}.</Text>
+        {/*Reason for embeding inside View is we can't add the style propert to the button*/}
+        <View style={styles.buttonContainer}>
+          <Button title={'Update Name'} onPress={()=>{setName('Viswanath Sarma Allamraju')}}/>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -31,6 +41,9 @@ const styles = StyleSheet.create({
     padding: 20,
     color: '#fff',
     fontWeight: "bold"
+  },
+  buttonContainer: {
+    padding: 20,
   }
 });
 
